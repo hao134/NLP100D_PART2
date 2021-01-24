90f7f0bc3232c1cb1147cf947d07faeb423a93ec
hao134 <40970293+hao134@users.noreply.github.com>
2021-01-24 14:46:43 +0800

    實作簡易word2vec

---
+++ DAY13/範例檔/utils/utility.py
@@ -0,0 +1,43 @@
+import numpy as np
+
+def remove_duplicate(params, grads):
+
+    params, grads = params[:], grads[:]  # copy list
+
+    while True:
+        find_flg = False
+        L = len(params)
+
+        for i in range(0, L - 1):
+            for j in range(i + 1, L):
+                # duplicate weights
+                if params[i] is params[j]:
+                    grads[i] += grads[j]  
+                    find_flg = True
+                    params.pop(j)
+                    grads.pop(j)
+                elif params[i].ndim == 2 and params[j].ndim == 2 and \
+                     params[i].T.shape == params[j].shape and np.all(params[i].T == params[j]):
+                    grads[i] += grads[j].T
+                    find_flg = True
+                    params.pop(j)
+                    grads.pop(j)
+
+                if find_flg: break
+            if find_flg: break
+
+        if not find_flg: break
+
+    return params, grads
+
+
+def clip_grads(grads, max_norm):
+    total_norm = 0
+    for grad in grads:
+        total_norm += np.sum(grad ** 2)
+    total_norm = np.sqrt(total_norm)
+
+    rate = max_norm / (total_norm + 1e-6)
+    if rate < 1:
+        for grad in grads:
+            grad *= rate
